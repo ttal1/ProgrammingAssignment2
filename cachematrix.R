8a82296471293849f8ad815e70247222c7d1962d
ttal <tamaraltal@gmail.com>
2015-11-16 20:37:01 -0500

    final code
    fixed incorrect variable

--- cachematrix.R
+++ cachematrix.R
@@ -78,5 +78,7 @@ cacheSolve <- function(x, ...) {
 ##testing functions
 ##my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
 ##my_matrix$get()
-##my_matrix$getInverse()
 ##cacheSolve(my_matrix)
+##> my_matrix$getInverse()
+##
+##
